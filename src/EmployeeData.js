17076a1763387c3cf92831c63cee4dbda3889534
Kaushal5233 <160103352+Kaushal5233@users.noreply.github.com>
2024-04-30 18:55:08 +0530

    my first commit reactwebsite

---
+++ src/EmployeeData.js
@@ -0,0 +1,25 @@
+
+
+export const EmployeeData = [
+    {
+        id : 1,
+        firstName : 'Kaushal',
+        lastName  : 'Parmar',
+        age       : 28,
+        Email   : 'parmarkaushal5233@gmail.com'
+    },
+    {
+        id : 2,
+        firstName : 'Hiren',
+        lastName  : 'Parmar',
+        age       : 26,
+        Email  : 'parmarhiren334@gmail.com'
+    },
+    {
+        id : 3,
+        firstName : 'Kiran',
+        lastName  : 'Parmar',
+        age       : 27,
+        Email  : 'parmarkiran12@gmail.com'
+    },
+]
